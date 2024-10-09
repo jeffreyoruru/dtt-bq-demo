b6d683c0a71d66e7979e5dbf8eaee77627095a46
Geoffrey Oruru <jeffrey.oruru@gmail.com>
2024-10-09 18:10:45 +0000

    Models updated

---
+++ models/example/stg_customers.sql
@@ -0,0 +1,11 @@
+With stg_customers AS (
+    select 
+        customer_id,
+        concat(first_name,' ', last_name) as customer_name,
+        email as email_address,
+        address as billing_address
+    from raw.customer
+
+)
+
+select * from stg_customers
